3d33ea237470bbe5fe980de41c86b4d97e3bba45
meo84 <oopsiday@gmail.com>
2017-02-15 14:20:52 +0100

    test reference content is present and contains at least 4 words

--- test/models/reference_test.rb
+++ test/models/reference_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ReferenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @reference = Reference.new(content: "Don't tell me the moon is shining; show me the glint of light on broken glass.")
+  end
+
+  test "should be valid" do
+    assert @reference.valid?
+  end
+
+  test "content should be present" do
+    @reference.content = "    "
+    assert_not @reference.valid?
+  end
+
+  test "content validation should accept contents with at least 4 words" do
+    valid_contents = ["good night and good luck", "don't mind the apostrophe"]
+    valid_contents.each do |valid_content|
+      @reference.content = valid_content
+      assert @reference.valid?, "#{valid_content.inspect} should be valid"
+    end
+  end
+
+  test "content validation should reject contents with less than 4 words" do
+    invalid_contents = ["oh hey", "it's me"]
+    invalid_contents.each do |invalid_content|
+      @reference.content = invalid_content
+      assert_not @reference.valid?, "#{invalid_content.inspect} should be invalid"
+    end
+  end
 end
